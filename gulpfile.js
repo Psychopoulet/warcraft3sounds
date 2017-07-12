7fde74bde8c35eb3611492c93077c2da1090a90a
DESKTOP-PVKHNCN\meona <svidal1@free.fr>
2017-07-12 18:25:31 +0200

    add base

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+
+"use strict";
+
+// deps
+
+	const path = require("path");
+
+	// gulp
+	const gulp = require("gulp");
+	const plumber = require("gulp-plumber");
+
+	// tests
+	const eslint = require("gulp-eslint");
+	const mocha = require("gulp-mocha");
+
+// private
+
+	var _unitTestsFiles = path.join(__dirname, "tests", "*.js");
+	
+	var _toTestFiles = [
+		path.join(__dirname, "gulpfile.js"),
+		path.join(__dirname, "lib", "*.js"),
+		_unitTestsFiles
+	];
+
+// tasks
+
+	gulp.task("eslint", () => {
+
+		return gulp.src(_toTestFiles)
+			.pipe(plumber())
+			.pipe(eslint({
+				"parserOptions": {
+					"ecmaVersion": 6
+				},
+				"rules": {
+					"linebreak-style": 0,
+					"quotes": [ 1, "double" ],
+					"indent": 0,
+					// "indent": [ 2, "tab" ],
+					"semi": [ 2, "always" ]
+				},
+				"env": {
+					"node": true, "es6": true, "mocha": true
+				},
+				"extends": "eslint:recommended"
+			}))
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError());
+
+	});
+
+	gulp.task("mocha", ["eslint"], () => {
+
+		return gulp.src(_unitTestsFiles)
+			.pipe(plumber())
+			.pipe(mocha({reporter: "spec"}));
+
+	});
+
+// watcher
+
+	gulp.task("watch", () => {
+		gulp.watch(_toTestFiles, ["mocha"]);
+	});
+
+
+// default
+
+	gulp.task("default", ["mocha"]);
+	
