d373b872ec9fd8f38ba1fce76e7129625c2ac97e
Sébastien VIDAL <meona@live.fr>
2025-03-31 04:00:27 +0200

    remove useless paths from api

--- lib/src/server/webRoutes.ts
+++ lib/src/server/webRoutes.ts
@@ -11,7 +11,7 @@
 // module
 
 export default function webRoutes (app: Express): void {
-	
+
 	// main page
 
 	app.get([ "/", "/public/index.html" ], (req: Request, res: Response): void => {
@@ -26,18 +26,6 @@ export default function webRoutes (app: Express): void {
 		return res.sendFile(join(__dirname, "..", "..", "..", "public", "bundle.js"));
 	}).get("/public/bundle.js.map", (req: Request, res: Response): void  => {
 		return res.sendFile(join(__dirname, "..", "..", "..", "public", "bundle.js.map"));
-	});
-
-	// libs
-
-	app.get("/public/jquery.min.js", (req: Request, res: Response): void  => {
-		return res.sendFile(join(__dirname, "..", "..", "..", "public", "jquery-3.2.1.min.js"));
-	}).get("/public/popper.min.js", (req: Request, res: Response): void  => {
-		return res.sendFile(join(__dirname, "..", "..", "..", "public", "popper-1.11.0.min.js"));
-	}).get("/public/bootstrap.min.js", (req: Request, res: Response): void  => {
-		return res.sendFile(join(__dirname, "..", "..", "..", "public", "bootstrap-4.0.0-beta.min.js"));
-	}).get("/public/angular.min.js", (req: Request, res: Response): void  => {
-		return res.sendFile(join(__dirname, "..", "..", "..", "public", "angular-1.6.5.min.js"));
 	});
 
 	// pictures
